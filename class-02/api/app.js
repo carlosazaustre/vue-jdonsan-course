62ff58fabcc38f421dba3b06c0463e7a27a5096f
Carlos Azaustre <cazaustre@gmail.com>
2018-12-17 15:37:36 +0100

    [class 2] get data for external API

---
+++ class-02/api/app.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    posts: [],
+    errors: []
+  },
+
+  created: function () {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => this.posts = response.data)
+      .catch(error => this.errors.push(error))
+  },
+
+  methods: {
+    sortArrayBy: function (type) {
+      return function (a, b) {
+        if (a[type] < b[type]) return -1
+        if (a[type] > b[type]) return 1
+        return 0
+      }
+    },
+    orderByTitle: function () {
+      this.posts.sort(this.sortArrayBy('title'))
+    },
+    orderById: function () {
+      this.defaultOrder()
+    },
+    defaultOrder: function () {
+      this.posts.sort(this.sortArrayBy('id'))
+    }
+  }
+
+})
