89d517c61f797dd5ff1ff56bee9ba8dc12954717
Carlos Azaustre <cazaustre@gmail.com>
2018-12-17 01:03:58 +0100

    [class-01] Components

--- class-01/app.js
+++ class-01/app.js
@@ -1,5 +1,18 @@
+// Global Component register
+Vue.component('hello-world', {
+  template: `
+    <h1>Hello, world! My name is {{ name }}</h1>
+  `,
+  data () {
+    return {
+      name: 'Carlos'
+    }
+  }
+})
+
 const app = new Vue({
   el: '#my-app',
+  // components: { HelloWorld } -- // Locally Component register
   data: {
     text: 'My first message on Vue'
   }
